1d9f26d14f07ca7263b4edf5f6778c64b630e3fd
wpacoder <arief.ke.1@hotmail.com>
2016-03-12 16:56:16 +0700

    Added files via upload

---
+++ TracksMoe/APIBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TracksMoe\APIBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('TracksMoeAPIBundle:Default:index.html.twig');
+    }
+}
---
+++ TracksMoe/APIBundle/TracksMoeAPIBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TracksMoe\APIBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TracksMoeAPIBundle extends Bundle
+{
+}
